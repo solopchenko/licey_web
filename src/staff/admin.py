e135ef2b57af3dc9c7efd1df2ba925c331c0e666
Svyatoslav Solopchenko <svyatoslav@MacBook-Air-Svyatoslav.local>
2017-01-19 17:40:10 +0300

    Добавлено приложение staff

--- src/staff/admin.py
+++ src/staff/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Person, PersonPosition, PersonTab
 # Register your models here.
+
+class PersonTabInline(admin.TabularInline):
+    model = PersonTab
+    extra = 0
+
+class PersonAdmin(admin.ModelAdmin):
+    fields = ('user', 'last_name', 'first_name', 'middle_name', 'office', 'positions', 'education', 'teaching_experience', 'photo', 'email', 'phone', )
+    inlines = (PersonTabInline, )
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(PersonPosition)
